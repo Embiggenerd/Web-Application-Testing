f71eba5af54923bf3dab7e93a6ac7c227e6660c2
igor <embiggenerd@gmail.com>
2020-01-16 18:38:32 -0500

    outs and innings working but not tested

--- client/src/App.js
+++ client/src/App.js
@@ -7,12 +7,16 @@ function App() {
   const [balls, setBalls] = useState(0)
   const [strikes, setStrikes] = useState(0)
   const [hits, setHits] = useState(0)
+  const [outs, setOuts] = useState(0)
+  const [innings, setInnings] = useState(0)
 
   const resetBoard = () => {
     setHits(0)
     setBalls(0)
     setStrikes(0)
   }
+
+
 
   const handleAddFoul = () => {
     if (strikes <= 1) {
@@ -31,25 +35,36 @@ function App() {
   const handleAddStrike = () => {
     setStrikes(strikes + 1)
   }
-
+  const strikeOut = () => {
+    resetBoard()
+    setOuts(outs + 1)
+  }
   useEffect(() => {
+
+
+
     if (balls >= 4) {
       resetBoard()
     }
 
     if (strikes >= 3) {
-      resetBoard()
+      strikeOut()
     }
 
     if (hits >= 1) {
       resetBoard()
     }
 
-  }, [balls, strikes, hits])
+    if (outs >= 6) {
+      setOuts(0)
+      setInnings(innings + 1)
+    }
+
+  }, [balls, strikes, hits, innings, outs])
 
   return (
     <div className="App" data-testid="app">
-      <Display balls={balls} strikes={strikes} />
+      <Display balls={balls} strikes={strikes} innings={innings} outs={outs} />
       <Dashboard handleAddFoul={handleAddFoul} handleAddBall={handleAddBall} handleAddStrike={handleAddStrike} handleAddHit={handleAddHit} />
     </div>
   );
